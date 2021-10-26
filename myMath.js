b6349807fb6ff761f519169521db85ad9e3b4525
giacoletti <giacoletti92@gmail.com>
2021-10-26 16:40:45 +0200

    Practicing arithmetic operations

---
+++ myMath.js
@@ -0,0 +1,76 @@
+let blueCars = 4;
+let redCars = 5;
+
+let totalCars = blueCars + redCars;
+
+console.log(`I have ${totalCars} cars!`);
+
+let tapasCount = 4;
+tapasCount++;
+
+console.log(`I have ${tapasCount} tapas!`);
+
+let bookCount = 10;
+bookCount += 20;
+
+console.log(`I have read ${bookCount} books!`);
+
+let bobsApples = 13;
+let stolenApples = 7;
+
+let remainingApples = bobsApples - stolenApples;
+
+console.log(`After getting robbed, Bob was left with ${remainingApples} apples.`);
+
+let waterBottles = 4;
+waterBottles--;
+
+console.log(`I am down to ${waterBottles} water bottles!`);
+
+let apples = 50;
+apples -= 30;
+
+console.log(`I have ${apples} apples left!`);
+
+let tacosPerPerson = 3;
+let people = 20;
+
+let tacosNeeded = tacosPerPerson * people;
+
+console.log(`We need ${tacosNeeded} tacos for the party.`);
+
+let boxes = 2;
+boxes *= 5;
+
+console.log(`I have ${boxes} boxes left!`);
+
+let totalSkittles = 75;
+let kids = 5;
+
+let skittlesPerKid = totalSkittles / kids;
+
+console.log(`Each kid gets ${skittlesPerKid} skittles.`);
+
+let peanuts = 100;
+peanuts /= 20;
+
+console.log(`I have ${peanuts} peanuts left!`);
+
+people = 20;
+let peoplePerCar = 6;
+
+let peopleInLastCar = people % peoplePerCar;
+
+console.log(`The last car will have ${peopleInLastCar} people in it.`);
+
+let slices = 45;
+slices %= 7;
+
+console.log(`I have ${slices} slices leftover!`);
+
+// Order of operations: PEMDAS:
+//Parentheses, Exponents, Multiply or Divide, Add or Subtract
+
+let x = (5 * 3) + 10 / 2; //15 + 10 / 2 = 15 + 5 = 20
+
+console.log(x);
