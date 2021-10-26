7183c84ee596a04da3353c6da29cfb02cf190657
giacoletti <giacoletti92@gmail.com>
2021-10-26 15:48:26 +0200

    Practicing variables, constants and data types

---
+++ variables_and_data_types.js
@@ -0,0 +1,40 @@
+let apples = 6;
+let weekday = "Thursday";
+
+console.log(`On ${weekday}, I ate ${apples} apples.`);
+
+const myConstant = 'Hello!';
+
+console.log(myConstant)
+
+let cars = 7;
+
+console.log(cars);
+
+console.log(typeof(cars));
+
+let city = "Gothenburg";
+
+console.log("The second largest city in Sweden is " + city + ".");
+
+console.log(`Look how
+    this string spans
+    over multiple lines!`);
+
+let favorite = "Pinchos";
+
+console.log(`My favorite family style restaurant is ${favorite}!`);
+
+let javaScriptIsInterpreted = true;
+
+let javaScriptIsDifficultToLearn = false;
+
+let nothing = null;
+
+let myVariable;
+
+console.log(myVariable);
+
+myVariable = undefined;
+
+console.log(myVariable);
